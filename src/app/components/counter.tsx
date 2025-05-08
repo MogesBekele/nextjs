6cf967785ca44f84bb64d81dcbfc5cfa12dc2ddb
MogesBekele <mogesbekele32@gmail.com>
2025-05-08 16:41:12 +0300

    use the client component for intractivite

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const Counter = () => {
   console.log("counter commpnent loaded")
   const [count, setCount] = useState(0);
-  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+  return <button onClick={() => setCount(count + 1)}>count: {count}</button>;
 };
 
 export default Counter;
