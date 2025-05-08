bbaf20cb30bca36341de6c4c1c62a7a6fbc66595
MogesBekele <mogesbekele32@gmail.com>
2025-05-08 16:21:36 +0300

    commit on the greet component

--- src/app/components/greet.tsx
+++ src/app/components/greet.tsx
@@ -3,7 +3,10 @@ export const Greet = ()=>{
   console.log("Greet component loaded")
     return (
         <div>
-            <h1>Greet</h1>
+           
+
+            <h1>Hello from Greet component</h1>
+            <p>This is a simple greeting component.</p>
         </div>
     )
 }
