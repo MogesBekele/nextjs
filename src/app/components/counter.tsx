d291bc0f9de1dbc368a0c8f086dff44a38ea82ac
MogesBekele <mogesbekele32@gmail.com>
2025-05-08 16:35:03 +0300

    commit on the counter componet

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react'
+"use client";
+import { useState } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState(0)
-  return (
-   <button onClick={()=>setCount(count+1)}>{count}</button>
-  )
-}
+  const [count, setCount] = useState(0);
+  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+};
 
-export default Counter
+export default Counter;
