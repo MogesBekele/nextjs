7291f4a800437863613c7c6b60135ec0e3d0b5d9
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 17:03:58 +0300

    add the active link on both navigation link

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -4,12 +4,12 @@ import {usePathname} from "next/navigation"
 export default function Navigation(){
   const pathname = usePathname()
   return(
-  <nav>
-   <Link className={pathname === "/"? "font-bold mr-4": "text-blue-500"} href="/">Home</Link>
-    <Link className={pathname === "/about"? "font-bold mr-4": "text-blue-500"} href="/about">About</Link>
-    <Link className={pathname === "/login"? "font-bold mr-4": "text-blue-500"} href="/login">Login</Link>
-    <Link className={pathname === "/register"? "font-bold mr-4": "text-blue-500"} href="/register">Register</Link>
-    <Link className={pathname === "/dashboard"? "font-bold mr-4": "text-blue-500"} href="/products/1">product 1</Link>
+  <nav className="flex justify-center items-center gap-4 p-4">
+   <Link className={pathname === "/"? "font-bold mr-4": "text-blue-500 mr-4"} href="/">Home</Link>
+    <Link className={pathname === "/about"? "font-bold mr-4": "text-blue-500 mr-4"} href="/about">About</Link>
+    <Link className={pathname === "/login"? "font-bold mr-4": "text-blue-500 mr-4"} href="/login">Login</Link>
+    <Link className={pathname === "/register"? "font-bold mr-4": "text-blue-500 mr-4"} href="/register">Register</Link>
+    <Link className={pathname === "/products/1"? "font-bold mr-4": "text-blue-500 mr-4"} href="/products/1">product 1</Link>
   
   </nav>
   )
