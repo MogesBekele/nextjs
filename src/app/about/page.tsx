f2a567f134414044ce25aa11e721a6412b085d51
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 17:16:27 +0300

    i almost  understand nextjs

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,8 @@ export default function About() {
     <div>
       <h1>About</h1>
       <p>This is the about page.</p>
-      <button className="text-blue-700 bg-amber-400 px-6 py-3 rounded-full" onClick={() => router.push("/blog/first-blog")}>Go to First Blog</button>
+      <button className="text-white  px-6 py-3 bg-blue-900 rounded-full" onClick={() => router.push("/blog/first-blog")}>Go to First Blog</button>
+      <button className="text-white px-6 py-3 bg-blue-900 rounded-full" onClick={() => router.push("/")}>Go Home</button>
     </div>
   );
 }
