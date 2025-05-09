9dd38b106ed8080aa9d6c12b9cd820ec9ee51a35
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 16:04:12 +0300

    create the dynamic route

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function Product (){
+    console.log("Product page loaded")
+    return (
+        <div>
+            <h1>Product</h1>
+            <p>This is the product page.</p>
+        </div>
+    )
+}
