e9094bdf515e1442b07059a4d2f705db529a3cd8
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 15:57:50 +0300

    create the blog component

---
+++ src/app/blog/first-blog/page.tsx
@@ -0,0 +1,9 @@
+export default function firsrBlog(){
+    console.log("first blog page loaded")
+    return (
+        <div>
+            <h1>First Blog</h1>
+            <p>This is the first blog page.</p>
+        </div>
+    )
+}
