0f6125ab53d25344b9f0a532bfa97912d26d4e62
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 16:58:24 +0300

    added the active link on the link

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -6,10 +6,10 @@ export default function Navigation(){
   return(
   <nav>
    <Link className={pathname === "/"? "font-bold mr-4": "text-blue-500"} href="/">Home</Link>
-    <Link className="mr-4 text-blue-500" href="/login">Login</Link>
-    <Link className="mr-4 text-blue-500" href="/register">Register</Link>
-    <Link className="mr-4 text-blue-500" href="/about">About</Link>
-    <Link className="mr-4 text-blue-500" href="/products/1">product 1</Link>
+    <Link className={pathname === "/about"? "font-bold mr-4": "text-blue-500"} href="/about">About</Link>
+    <Link className={pathname === "/login"? "font-bold mr-4": "text-blue-500"} href="/login">Login</Link>
+    <Link className={pathname === "/register"? "font-bold mr-4": "text-blue-500"} href="/register">Register</Link>
+    <Link className={pathname === "/dashboard"? "font-bold mr-4": "text-blue-500"} href="/dashboard">Dashboard</Link>
   
   </nav>
   )
