0839d8d13fb11a61c1ced281ab3c0eb0656fbc5e
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 16:46:47 +0300

    added the on the navigation component

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,12 +2,11 @@ import Link from "next/link"
 export default function Navigation(){
   return(
   <nav>
-    <ul>
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/about">About</Link></li>
-      <li><Link href="/register">Register</Link></li>
-      <li><Link href="/login">Login</Link></li>
-    </ul>
+   <Link className="mr-4 text-blue-500" href="/">Home</Link>
+    <Link className="mr-4 text-blue-500" href="/login">Login</Link>
+    <Link className="mr-4 text-blue-500" href="/register">Register</Link>
+    <Link className="mr-4 text-blue-500" href="/about">About</Link>
+    <Link className="mr-4 text-blue-500" href="/product/1">product 1</Link>
   
   </nav>
   )
