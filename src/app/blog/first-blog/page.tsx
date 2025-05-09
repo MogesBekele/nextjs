cc1b1d004b3e8df7da490a7361fe0969d5f5d3ef
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 16:01:10 +0300

    create the dynamic route

--- src/app/blog/first-blog/page.tsx
+++ src/app/blog/first-blog/page.tsx
@@ -7,3 +7,6 @@ export default function firsrBlog(){
         </div>
     )
 }
+
+
+// nested routes
--- src/app/components/greet.tsx
+++ src/app/components/greet.tsx
@@ -1,12 +1,9 @@
-export const Greet = ()=>{
-
-  console.log("Greet component loaded")
-    return (
-        <div>
-           
-
-            <h1>Hello from Greet component</h1>
-            <p>This is a simple greeting component.</p>
-        </div>
-    )
-}
+export const Greet = () => {
+  console.log("Greet component loaded");
+  return (
+    <div>
+      <h1>Hello from Greet component</h1>
+      <p>This is a simple greeting component.</p>
+    </div>
+  );
+};
