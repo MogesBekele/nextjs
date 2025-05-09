518c7cfc0f52fcb1809ca69307cc02ebf60e8220
MogesBekele <mogesbekele32@gmail.com>
2025-05-09 16:57:38 +0300

    commit on the active link

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,9 +2,10 @@
 import Link from "next/link"
 import {usePathname} from "next/navigation"
 export default function Navigation(){
+  const pathname = usePathname()
   return(
   <nav>
-   <Link className="mr-4 text-blue-500" href="/">Home</Link>
+   <Link className={pathname === "/"? "font-bold mr-4": "text-blue-500"} href="/">Home</Link>
     <Link className="mr-4 text-blue-500" href="/login">Login</Link>
     <Link className="mr-4 text-blue-500" href="/register">Register</Link>
     <Link className="mr-4 text-blue-500" href="/about">About</Link>
