a687fa5842cb0c89a534b0b5f1f79dae5dd1e288
MogesBekele <mogesbekele32@gmail.com>
2025-05-10 16:54:29 +0300

    create the error component

--- src/app/users-server/error.tsx
+++ src/app/users-server/error.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+"use client"
 
-const Error = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import { useEffect } from "react"
+
+const Error = ({error}: {error:Error}) => {
+ useEffect(()=>{
+  console.log("Error component mounted")
+  console.log(error)
+ },[error])
+
+ return (
+  <div>
+   <h1 className="text-2xl font-bold mb-4">Error</h1>
+   <p className="text-red-500">{error.message}</p>
+  </div>
+ )
 }
 
 export default Error
