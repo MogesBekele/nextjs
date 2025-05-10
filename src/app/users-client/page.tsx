27b1d82b0f056308ceecb48a15300699172d72b3
MogesBekele <mogesbekele32@gmail.com>
2025-05-10 16:36:25 +0300

    fetch data from the api

--- src/app/users-client/page.tsx
+++ src/app/users-client/page.tsx
@@ -1,29 +1,58 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 
-type User ={
+type User = {
   id: number;
   name: string;
   username: string;
   email: string;
   phone: string;
-}
+};
 
-export default function UsersClient(){
-  const [users, setusers] = useState<User[]>([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState('')
+export default function UsersClient() {
+  const [users, setusers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-  useEffect(()=>{
-    async function fetchUsers(){
+  useEffect(() => {
+    async function fetchUsers() {
       try {
-        const response= await fetch()
-        
+        const response = await fetch(
+          "http://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const data = await response.json();
+        setusers(data);
       } catch (error) {
-        
+        setError("Failed to fetch users");
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }finally{
+        setLoading(false);
       }
     }
-  })
+    fetchUsers();
+  }, []);
 
-
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Users</h1>
+      <ul className="list-disc pl-5">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
