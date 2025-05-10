88a8f4ee05f51a59657344bd6cbb675d273701f8
MogesBekele <mogesbekele32@gmail.com>
2025-05-10 16:16:38 +0300

    chenk the endpoint

--- src/app/users/[id]/route.ts
+++ src/app/users/[id]/route.ts
@@ -5,3 +5,14 @@ export async function GET(_request:Request, { params}: {params:{id:string}}){
   const user  = users.find((user)=> user.id===parseInt(id))
   return Response.json(user)
 }
+
+export async function PUT(request:Request, {params}: {params:{id:string}}){
+  const {id} = await params
+  const user = await request.json()
+  const index = users.findIndex((user)=> user.id===parseInt(id))
+  if(index===-1){
+    return new Response("User not found", {status:404})
+  }
+  users[index] = {...users[index], ...user}
+  return Response.json(users[index])
+}
