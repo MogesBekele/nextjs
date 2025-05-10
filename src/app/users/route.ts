6c2d53824d2f00154f991d8ced67861c24df003b
MogesBekele <mogesbekele32@gmail.com>
2025-05-10 15:58:26 +0300

    check on the pastman

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -6,3 +6,9 @@ export async function GET() {
 return Response.json(users);
 }
 
+export async function POST(request: Request) {
+  const user = await request.json();
+  const newUser = { id: users.length + 1, name: user.name };
+  users.push(newUser);
+  return Response.json(newUser);
+}
