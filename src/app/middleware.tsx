695acc35b678c7ce74d5e33aabdfad451b5c397a
MogesBekele <mogesbekele32@gmail.com>
2025-05-11 17:27:16 +0300

    commit in the protected route

--- src/app/middleware.tsx
+++ src/app/middleware.tsx
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isProtectedRoute = createRouteMatcher(["/mock-users"])
-export default clerkMiddleware()
+export default clerkMiddleware(async (auth, req)=>{
+  if (isProtectedRoute(req)) await auth.protect();
+
+})
 
 export const config = {
   matcher: [
