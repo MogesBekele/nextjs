6380e99a8c4c89ca717899f1992caa32f829632d
MogesBekele <mogesbekele32@gmail.com>
2025-05-11 17:53:36 +0300

    check the loaded

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { useState } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
 
 const Counter = () => {
+  const {isLoaded, userId, sessionId, getToken} = useAuth();
+
+ 
   console.log("counter commpnent loaded")
   const [count, setCount] = useState(0);
+
+   if (!isLoaded || !userId) {
+    return null;
+  }
   return <button onClick={() => setCount(count + 1)}>count: {count}</button>;
 };
 
