468c62275471692ce0e0e218d3d72fc58cb3aec8
MogesBekele <mogesbekele32@gmail.com>
2025-05-11 17:49:13 +0300

    commit on the middleware

--- src/app/middleware.tsx
+++ src/app/middleware.tsx
@@ -1,17 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Match exact route or subroutes of /mock-users
-const isProtectedRoute = createRouteMatcher(["/mock-users"]);
+// Match /mock-users and its subroutes
+const isProtectedRoute = createRouteMatcher(["/mock-users(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect(); // Protect the route and redirect to sign-in if not authenticated
+    await auth.protect(); // Automatically redirects to sign-in if not authenticated
   }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files
+    // Skip Next.js internals and static files
     "/((?!_next|.*\\..*).*)",
     "/(api|trpc)(.*)",
   ],
