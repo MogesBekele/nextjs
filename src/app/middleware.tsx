612f7851ce1d106a003857100c074256bf7153ad
MogesBekele <mogesbekele32@gmail.com>
2025-05-11 17:24:02 +0300

    athorize the app

--- src/app/middleware.tsx
+++ src/app/middleware.tsx
@@ -1,5 +1,5 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
-
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+const isProtectedRoute = createRouteMatcher(["/mock-users"])
 export default clerkMiddleware()
 
 export const config = {
