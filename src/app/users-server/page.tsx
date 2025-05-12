7862f0cdfc2a1acfb525775d4d2dbdb941304b2f
MogesBekele <mogesbekele32@gmail.com>
2025-05-12 16:02:38 +0300

    improve the client page

--- src/app/users-server/page.tsx
+++ src/app/users-server/page.tsx
@@ -8,15 +8,20 @@ type User = {
 
 export default async function UsersServer() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch("http://jsonplaceholder.typicode.com/users");
 
-  const users = await response.json();
+  const response = await fetch("http://jsonplaceholder.typicode.com/users");
+  
+  if (!response.ok) {
+    throw new Error("Failed to fetch users");
+  }
+
+  const users: User[] = await response.json();
 
   return (
     <ul className="list-disc pl-5">
-      {users.map((user: User) => (
+      {users.map((user) => (
         <li key={user.id} className="mb-2">
-          <strong>{user.name}</strong> 
+          <strong>{user.name}</strong>
         </li>
       ))}
     </ul>
