1071ab85ddee6158183c21c2eca76c5750c14c70
MogesBekele <mogesbekele32@gmail.com>
2025-05-12 16:42:37 +0300

    footer component created

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>footer</div>
+   
+    <footer className="bg-gray-900 text-white p-4 mt-12">
+      <p>Welcome to the footer section of the browser</p>
+      <p>All rights reserved &copy; {new Date().getFullYear()}</p>
+    </footer>
   )
 }
 
