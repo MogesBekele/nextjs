1f30882be3af01ac9994e924c5ed05ed239a6a9a
MogesBekele <mogesbekele32@gmail.com>
2025-05-13 17:24:36 +0300

    add image in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Greet } from "./components/greet";
 import Counter from "./components/counter";
+import Image from "next/image";
 
 const page = () => {
   return (
     <div>
+      <Image 
+        src="/https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&psig=AOvVaw3R3JIhk9lFT1kFvoLwlGXd&ust=1747232565638000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKihotfSoI0DFQAAAAAdAAAAABAE" // Replace with your actual image path or URL
+        alt="Description of image"
+        width={500}
+        height={300}
+      />
+
       <Greet />
       <Counter />
     </div>
