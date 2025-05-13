18531d0bc197714c4a0770acf7a390141a570057
MogesBekele <mogesbekele32@gmail.com>
2025-05-13 20:47:18 +0300

    add the layout on the users component

---
+++ src/app/products/friends/page.tsx
@@ -0,0 +1,12 @@
+export default async function Product (){
+
+
+    return (
+        <div>
+            <h1>friendes page</h1>
+            <p>This is the product page.</p>
+        </div>
+    )
+}
+
+// dynamic route
---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default async function Product() {
+    return (
+        <div>
+            <h1>Product: page</h1>
+            <p>This is the product page.</p>
+        </div>
+    )
+}
+
+// dynamic route
