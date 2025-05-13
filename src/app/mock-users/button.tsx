51ba4cfb8f991fc202892e76c9d55c02598c3785
MogesBekele <mogesbekele32@gmail.com>
2025-05-13 18:08:47 +0300

    added the hover on click me button

--- src/app/mock-users/button.tsx
+++ src/app/mock-users/button.tsx
@@ -1,12 +1,18 @@
-"use client"
+"use client";
 
 function Button() {
   return (
     <div>
-      <button className="bg-blue-800 px-6 py-3 rounded-full mt-10" onClick={()=>{alert("click me")}}>click me</button>
-      
+      <button
+        className="bg-blue-800 px-6 py-3 rounded-full mt-10 hover:cursor-pointer "
+        onClick={() => {
+          alert("click me");
+        }}
+      >
+        click me
+      </button>
     </div>
-  )
+  );
 }
 
-export default Button
+export default Button;
