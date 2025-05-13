e1216039bc3fab400e197bec4d4b949f703075ff
MogesBekele <mogesbekele32@gmail.com>
2025-05-13 17:36:58 +0300

    fix image issue on the page

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
         hostname: "upload.wikimedia.org",
-        port: "",
-        pathname: "/**",
+     
       },
     ],
   },
