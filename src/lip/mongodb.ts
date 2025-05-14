31c57d5df6c14660de823d10bedfe52a30fe20a3
MogesBekele <mogesbekele32@gmail.com>
2025-05-14 16:05:08 +0300

    mongodb logic

--- src/lip/mongodb.ts
+++ src/lip/mongodb.ts
@@ -1,1 +1,19 @@
 import { MongoClient } from "mongodb";
+
+const uri = process.env.mongo_url;
+const options = {};
+
+let client;
+let clientPromise;
+
+if (!process.env.mongo_uri) {
+  throw new Error("please connect the uri");
+}
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
