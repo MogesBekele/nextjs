4e12792d348c5887a56803d0ddf6493a2f2864a5
MogesBekele <mogesbekele32@gmail.com>
2025-05-14 16:13:07 +0300

    mongodb connection

--- src/lip/mongodb.ts
+++ src/lip/mongodb.ts
@@ -1,14 +1,15 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI; // ✅ consistent name
 const options = {};
 
 let client;
 let clientPromise;
 
 if (!uri) {
-  throw new Error("please connect the uri");
+  throw new Error("Please add MONGODB_URI to your .env.local file");
 }
+
 if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options);
   global._mongoClientPromise = client.connect();
